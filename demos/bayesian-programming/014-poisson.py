421584e2e2f9fa8ebcb9a047ec9ae0eaf445fac2
herereadthis <herereadthis@gmail.com>
2019-01-09 22:54:49 -0500

    poisson

---
+++ demos/bayesian-programming/014-poisson.py
@@ -0,0 +1,16 @@
+from IPython.core.pylabtools import figsize
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+
+figsize(12.5, 3.5)
+
+count_data = np.loadtxt("demos/bayesian-programming/014-poisson.csv")
+n_count_data = len(count_data)
+plt.bar(np.arange(n_count_data), count_data, color="#348ABD")
+plt.xlabel("Time (days)")
+plt.ylabel("count of text-msgs received")
+plt.title("Did the user's texting habits change over time?")
+plt.xlim(0, n_count_data)
+
+plt.show()
--- docs/bayesian-programming.md
+++ docs/bayesian-programming.md
@@ -10,3 +10,4 @@ Notes from Probabilistic Programming and Bayesian Methods for Hackers
 * What is the probability the application has no bugs, given the results of the debugging test?
   > ![Bayes' Theorem](./img/d09541ec-5a36-4035-9cb2-192a52f5324c.png)
   * Let Tests be <em>A</em> and let the probability of no bugs be <em>B</em>
+  * Let <em>P(A|B)</em> be the probability of no bugs (B), given A test passes
