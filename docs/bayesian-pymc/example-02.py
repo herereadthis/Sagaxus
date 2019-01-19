700034b1df563599ee0a8b0e2d722c27ebf51687
herereadthis <herereadthis@gmail.com>
2019-01-18 23:40:38 -0500

    clean up example

--- docs/bayesian-pymc/example-02.py
+++ docs/bayesian-pymc/example-02.py
@@ -1,12 +1,14 @@
+'''
+Render various Beta distributions with Seaborn
+'''
 import numpy as np
 from scipy.stats import beta
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
-if __name__ == "__main__":
-    sns.set_palette("deep", desat=.6)
-    sns.set_context(rc={"figure.figsize": (8, 4)})
+if __name__ == '__main__':
+    sns.set_palette('deep', desat=.6)
+    sns.set_context(rc={'figure.figsize': (8, 4)})
     x = np.linspace(0, 1, 100)
     params = [
         (0.5, 0.5),
@@ -17,8 +19,9 @@ if __name__ == "__main__":
     ]
     for p in params:
         y = beta.pdf(x, p[0], p[1])
-        plt.plot(x, y, label="$\\alpha=%s$, $\\beta=%s$" % p)
-    plt.xlabel("$\\theta$, Fairness")
-    plt.ylabel("Density")
-    plt.legend(title="Parameters")
+        label = '$\\alpha={p[0]}$, $\\beta={p[1]}$'.format(p=p)
+        plt.plot(x, y, label=label)
+    plt.xlabel('$\\theta$, Fairness')
+    plt.ylabel('Density')
+    plt.legend(title='Parameters')
     plt.show()
